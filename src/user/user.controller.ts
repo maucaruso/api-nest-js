0f386be70132aa025d91cea81b0c536d4929815a
maucaruso <carusojr@outlook.com>
2023-04-26 08:48:41 -0300

    feat: creating user crud routes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,53 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Param,
+  Put,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UserController {
   @Post()
   async create(@Body() body) {
     return { body };
+  }
+
+  @Get()
+  async list() {
+    return { users: [] };
+  }
+
+  @Get(':id')
+  async show(@Param() param) {
+    return { user: {}, param };
+  }
+
+  @Put(':id')
+  async update(@Body() body, @Param() param) {
+    return {
+      method: 'PUT',
+      body,
+      param,
+    };
+  }
+
+  @Patch(':id')
+  async updatePartial(@Body() body, @Param() param) {
+    return {
+      method: 'PATCH',
+      body,
+      param,
+    };
+  }
+
+  @Delete(':id')
+  async delete(@Param() param) {
+    return {
+      param,
+    };
   }
 }
