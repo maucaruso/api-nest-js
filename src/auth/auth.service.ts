a64bb2d35a1ed9bf3e9cfd970cb8b8e1c23aa47e
maucaruso <carusojr@outlook.com>
2023-05-07 18:23:44 -0300

    refactor: removing async from non async methods

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -17,7 +17,7 @@ export class AuthService {
     private readonly userService: UserService,
   ) {}
 
-  async createToken(user: User) {
+  createToken(user: User) {
     return {
       accessToken: this.jwtService.sign(
         {
@@ -35,7 +35,7 @@ export class AuthService {
     };
   }
 
-  async checkToken(token: string) {
+  checkToken(token: string) {
     try {
       const data = this.jwtService.verify(token, {
         audience: 'users',
@@ -48,7 +48,7 @@ export class AuthService {
     }
   }
 
-  async isValidToken(token: string) {
+  isValidToken(token: string) {
     try {
       this.checkToken(token);
       return true;
