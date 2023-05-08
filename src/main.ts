63806a94168431c3f96b3ecbf39298f12fd95f61
maucaruso <carusojr@outlook.com>
2023-05-07 22:02:11 -0300

    chore: enabling cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { LogInterceptor } from './interceptors/log.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    methods: ['GET'],
+  });
+
   app.useGlobalPipes(new ValidationPipe());
 
   app.useGlobalInterceptors(new LogInterceptor());
